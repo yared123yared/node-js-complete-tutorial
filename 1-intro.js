f16c0be1274349b5c80a724784e1211879edec7b
=yared123yared <=yaredyaya16@gmail.com>
2021-07-10 14:30:42 +0300

    initial commit

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+// const amount=9
+
+// if(amount < 10){
+//     console.log('small number');
+// }else{
+//     console.log('large number');
+// }
+
+// console.log("hey this is my first node application");
+
+ 
+console.log(__dirname);

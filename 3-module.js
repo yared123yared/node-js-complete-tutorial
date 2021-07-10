7c9a111ceef97ad0dcdd994c0736420507f3c7f7
=yared123yared <=yaredyaya16@gmail.com>
2021-07-10 15:42:53 +0300

    adding od module'

---
+++ 3-module.js
@@ -0,0 +1,13 @@
+// CommonJS, every file is module(by default)
+// modules -Encapsulated Code (only share minimum)
+const names=require('./4-names');
+const sayHi=require('./utils');
+const data=require('./6-alternative-flavore')
+require('./7-mind-grenade')
+
+
+
+// sayHi("yared");
+// sayHi("john");
+// sayHi(names.john);
+// sayHi(names.peter);
